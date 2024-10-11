6888acbccf1a0e5d026a6f7f77cfb45699d9789e
joaocarloslima <joaocarloslima@me.com>
2024-10-11 14:03:38 -0300

    version 2

--- WorkshopGithub/ContentView.swift
+++ WorkshopGithub/ContentView.swift
@@ -2,20 +2,24 @@
 //  ContentView.swift
 //  WorkshopGithub
 //
-//  Created by Joao Carlos Lima on 11/10/24.
+//  Created by Joao Carlos Lima on 02/10/24.
 //
 
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        TabView {
+            FirstView()
+                .tabItem {
+                    Label("First", systemImage: "1.circle")
+                }
+            
+            SecondView()
+                .tabItem {
+                    Label("Second", systemImage: "2.circle")
+                }
         }
-        .padding()
     }
 }
 
