b613559db11634a3140227c83ae7dd1e74d445df
joaocarloslima <joaocarloslima@me.com>
2024-10-11 13:57:03 -0300

    Initial Commit

---
+++ WorkshopGithub/WorkshopGithubApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkshopGithubApp.swift
+//  WorkshopGithub
+//
+//  Created by Joao Carlos Lima on 11/10/24.
+//
+
+import SwiftUI
+
+@main
+struct WorkshopGithubApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
